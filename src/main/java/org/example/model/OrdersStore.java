a584aa32d39dd2b0eb1a3e445dcaa7198d084e61
aleksandra-gonta <aleksandra.gonta@gmail.com>
2020-02-10 12:07:47 +0100

    Create enum Category and Order, OrderStore and Product classes

---
+++ src/main/java/org/example/model/OrdersStore.java
@@ -0,0 +1,17 @@
+package org.example.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+public class OrdersStore {
+    private Set<Order> orders;
+}
---
+++ src/main/java/org/example/model/Product.java
@@ -0,0 +1,20 @@
+package org.example.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.example.model.enums.Category;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Product {
+    private String name;
+    private BigDecimal price;
+    private Category category;
+
+}
