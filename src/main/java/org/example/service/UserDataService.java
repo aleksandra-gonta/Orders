c1487f14889de10d9451ffe8d4173a0d3ee7eb53
aleksandra-gonta <aleksandra.gonta@gmail.com>
2020-02-13 10:41:34 +0100

    Create User Data Service class

---
+++ src/main/java/org/example/service/UserDataService.java
@@ -0,0 +1,68 @@
+package org.example.service;
+
+import org.example.exception.AppException;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+
+public final class UserDataService {
+
+    private UserDataService() {}
+
+    private final static Scanner scanner = new Scanner(System.in);
+
+    public static String getString(String message) {
+        System.out.println(message);
+        return scanner.nextLine();
+    }
+
+    public static int getInt(String message) {
+        System.out.println(message);
+
+        String valueAsString = scanner.nextLine();
+        if (!valueAsString.matches("\\d+")) {
+            throw new AppException("int value is not correct");
+        }
+
+        return Integer.parseInt(valueAsString);
+    }
+    public static double getDouble(String message) {
+        System.out.println(message);
+
+        String valueAsString = scanner.nextLine();
+        if (!valueAsString.matches("\\d+\\.*\\d+")) {
+            throw new AppException("double value is not correct");
+        }
+
+        return Double.parseDouble(valueAsString);
+    }
+    public static BigDecimal getBigDecimal(String message) {
+        System.out.println(message);
+
+        String valueAsString = scanner.nextLine();
+        if (!valueAsString.matches("\\d+\\.*\\d+")) {
+            throw new AppException("Big Decimal value is not correct");
+        }
+
+        return BigDecimal.valueOf(Long.parseLong(valueAsString));
+    }
+
+    public static LocalDate getDate(String message) {
+        try {
+            System.out.println(message);
+            String value = scanner.nextLine();
+            return LocalDate.parse(value, DateTimeFormatter.ofPattern("d-MM-yyyy"));
+        } catch (Exception e) {
+            throw new AppException("date format is not correct: " + e.getMessage());
+        }
+    }
+
+    public static void close() {
+        if (scanner != null) {
+            scanner.close();
+        }
+    }
+}
